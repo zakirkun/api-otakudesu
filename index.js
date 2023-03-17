cf66b8d7ec1bc1c2b7bbb6ca12865b9c5af3c595
Zakir <coderzakir44@gmail.com>
2023-03-17 21:12:14 +0700

    update

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const route = require("./src/router/route")
+const { inject } = require("@vercel/analytics")
+
+inject();
 
 app.use(cors());
 app.use(route)
